373c3121c3b46c810bc9bae027b85e8c7791779b
Kun Chen <kc75997@uga.edu>
2016-09-25 03:46:23 -0400

    new parsing approach

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var imageuri = ""
 //parse the dollar amount in the transaction amount textfield
 function parseTextField() {
     transactionAmount = parseFloat($("#deposit").text());
-    console.log(transactionAmount)
+    transactionAmount = document.getElementById('#deposit').value
 
     initiateEmotionDectectionAPI();
 }
