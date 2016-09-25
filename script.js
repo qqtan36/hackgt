de0b5f6fa74721bf3f27c85b4d9b648ddf9e7fba
Kun Chen <kc75997@uga.edu>
2016-09-25 02:06:42 -0400

    8

--- script.js
+++ script.js
@@ -21,3 +21,26 @@ function updateCameraStatusLabelToTrue() {
 //take picture of the user
 function takePicOfTheUser() {
 }
+
+function updateBalanceLabel() {
+    $.ajax({
+        url: "http://api.reimaginebanking.com/accounts?type=Checking&key=e2352ee557f1453da0a2eb28fbc5c5a7",
+        async: true,
+        beforeSend: function(xhrObj){
+            // Request headers
+            //xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("Content-Type","application/json");
+        },
+        type: "GET",
+        // Request body
+        //data: "{'url': 'https://i.imgur.com/pbb73Gs.jpg'}",
+        //data: "{'url':" + imageuri + "}"
+    })
+    .done(function(data) {
+        var userBalance = data[0]["balance"].toFixed(2);
+        $.find(".table-bordered").find("td:eq(1)").text(userBalance)
+    })
+    .fail(function() {
+        console.log("error in updatingBalanceLabel");
+    });
+}
