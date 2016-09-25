0b99d05521bd619b5999dcf0cf7ff26868721467
Kun Chen <kc75997@uga.edu>
2016-09-25 02:19:52 -0400

    12

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function updateBalanceLabel() {
     })
     .done(function(data) {
         var userBalance = data[0]["balance"].toFixed(2);
-        $.find(".table-bordered").find("td").eq(1).text(userBalance)
+        $.find(".table-bordered").find(".td").eq(1).text(userBalance)
     })
     .fail(function() {
         console.log("error in updatingBalanceLabel");
